1855924bb16c4e4df9edb6b8e33f105dd7bab9b7
travis-john <travis.wl.john@gmail.com>
2020-03-18 13:54:17 -0400

    added pages for sorting - router working

---
+++ src/pages/All.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Row from '../components/Row';
+import Col from '../components/Col';
+import Card from '../components/Card';
+
+class All extends Component {
+
+  state = {
+    results: [],
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    this.setState({ results: res.data })
+  }
+
+  render() {
+    return (
+      <Row>
+      {this.state.results && this.state.results.map(result => (
+        <Col size='md-4'>
+          <Card
+            key={result.id}
+            name={result.name}
+            company={result.company.name}
+            email={result.email}
+            city={result.address.city}
+            phone={result.phone}
+            />
+        </Col>
+      ))}
+      </ Row>
+    );
+  }
+}
+
+export default All;
