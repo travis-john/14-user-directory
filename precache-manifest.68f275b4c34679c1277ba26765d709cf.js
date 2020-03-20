9e092a5db0fa69f38cec9771f77bd52e86aeb14c
travis-john <travis.wl.john@gmail.com>
2020-03-20 13:15:57 -0400

    Updates

---
+++ precache-manifest.68f275b4c34679c1277ba26765d709cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549442ce319da4a97c2df8a740342160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3dc6c1540b255d6c1f7",
+    "url": "/static/js/2.ec63db8c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ec63db8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4048f413348d34853779",
+    "url": "/static/js/main.44e842b0.chunk.js"
+  },
+  {
+    "revision": "886bd0d3a589605b4508",
+    "url": "/static/js/runtime-main.dbbd4fbc.js"
+  }
+]);
